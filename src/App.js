f941122675b2756870d094c657af112981b3a8d9
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-19 08:55:31 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>On + It</h1>
+    <p>A simple progress tracking app</p>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Register</li>
+        <li>Login</li>
+      </ul>
+    </nav>
     </div>
   );
 }
