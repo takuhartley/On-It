d99def383539ac09c00bbae695dbab6faf6e9154
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-02-27 21:40:28 +0900

    Updated

---
+++ Server/models/Profile.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  // Reference to user model
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String
+  },
+  interests: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instragram: {
+      type: String
+    }
+  },
+  date: {
+      type: Date,
+      default: Date.now
+  }
+});
--- Server/routes/api/user.js
+++ Server/routes/api/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
+const config = require("config");
 // User models
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route      POST api/users
 // @desc       Register user
@@ -40,6 +42,13 @@ router.post(
           .json({ errors: [{ msg: "User already exists" }] });
       }
 
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
       user = new User({
         name,
         email,
@@ -53,7 +62,22 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        // Experation
+        { expiresIn: 360000 },
+        // Error
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       // Server error
       console.log(err.message);
